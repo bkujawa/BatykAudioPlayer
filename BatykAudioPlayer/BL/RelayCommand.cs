87252252aadbd132739e9c8fa829359e9a402d00
Bartosz Kujawa <bakj@dgs.com>
2019-05-02 09:57:47 +0200

    Added RelayCommand class which implements ICommand interface.

---
+++ BatykAudioPlayer/BL/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BatykAudioPlayer.BL
+{
+    public class RelayCommand : ICommand
+    {
+        // Fields
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        // Constructors
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+        // Members
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
