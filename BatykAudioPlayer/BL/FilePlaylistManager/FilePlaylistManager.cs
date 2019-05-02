8dad42adbc62268c1a3e5c5dd9dba791e5a6d2b4
Bartosz Kujawa <bakj@dgs.com>
2019-05-02 13:30:51 +0200

    Blank FilePlayListManager class with an interface.

---
+++ BatykAudioPlayer/BL/FilePlaylistManager/FilePlaylistManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatykAudioPlayer.BL.FilePlaylistManager
+{
+    public class FilePlaylistManager : IFilePlaylistManager
+    {
+        public void FillPlaylist()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FillSoundsDirectory(string dirPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetDefaultDirectory(string dirPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetDefualtPlaylist(string listPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BatykAudioPlayer/BL/FilePlaylistManager/IFilePlaylistManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatykAudioPlayer.BL.FilePlaylistManager
+{
+    public interface IFilePlaylistManager
+    {
+        void FillSoundsDirectory(string dirPath);
+        void FillPlaylist();
+        void SetDefaultDirectory(string dirPath);
+        void SetDefualtPlaylist(string listPath);
+
+    }
+}
