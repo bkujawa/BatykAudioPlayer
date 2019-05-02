da5a9dcbf13193463b056ed5e4c38463fe490c43
Bartosz Kujawa <bakj@dgs.com>
2019-05-02 14:48:44 +0200

    Implemented FillSoundsDirectory method.

--- BatykAudioPlayer/BL/FilePlaylistManager/FilePlaylistManager.cs
+++ BatykAudioPlayer/BL/FilePlaylistManager/FilePlaylistManager.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BatykAudioPlayer.APP;
+using BatykAudioPlayer.APP.AudioPlayer;
+using BatykAudioPlayer.BL.SoundEngine;
 
 namespace BatykAudioPlayer.BL.FilePlaylistManager
 {
     public class FilePlaylistManager : IFilePlaylistManager
     {
+        #region Private fields
+
+        private AudioPlayerViewModel APViewModel;
+
+        #endregion
+
+        #region IFilePlaylistManager implementation
         public void FillPlaylist()
         {
             throw new NotImplementedException();
@@ -15,7 +26,21 @@ namespace BatykAudioPlayer.BL.FilePlaylistManager
 
         public void FillSoundsDirectory(string dirPath)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrEmpty(dirPath))
+            {
+                var allFiles = Directory.GetFiles(dirPath);
+                var soundList = new List<Sound>();
+                foreach (var file in allFiles)
+                {
+                    var pathExtension = Path.GetExtension(file);
+                    if (pathExtension?.ToUpper() == ".MP3")
+                    {
+                        soundList.Add(new Sound(Path.GetFileNameWithoutExtension(file), file));
+                    }
+                }
+                APViewModel.Sounds.Clear();
+                soundList.ForEach(c => APViewModel.Sounds.Add(c));
+            }
         }
 
         public void SetDefaultDirectory(string dirPath)
@@ -27,5 +52,30 @@ namespace BatykAudioPlayer.BL.FilePlaylistManager
         {
             throw new NotImplementedException();
         }
+
+        #endregion
+
+        #region Constructor
+
+        public FilePlaylistManager(object sender)
+        {
+            SetViewModel(sender);
+        }
+
+        #endregion
+
+        #region Private helpers
+
+        private void SetViewModel(object sender)
+        {
+            switch (sender)
+            {
+                case AudioPlayerViewModel apvm:
+                    APViewModel = (AudioPlayerViewModel)apvm;
+                    break;
+            }
+        }
+
+        #endregion
     }
 }
