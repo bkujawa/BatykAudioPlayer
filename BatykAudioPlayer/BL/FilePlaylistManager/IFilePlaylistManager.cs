c5fe1d0089b423a8cb5ec98aa2e8c855aeb8ccb6
Bartosz Kujawa <bkujawa@gmail.com>
2019-05-04 15:43:11 +0200

    Changed naming and return types of FillSoundsFromDirectory and FillPlaylist methods.

--- BatykAudioPlayer/BL/FilePlaylistManager/IFilePlaylistManager.cs
+++ BatykAudioPlayer/BL/FilePlaylistManager/IFilePlaylistManager.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BatykAudioPlayer.APP.AudioPlayer;
+using BatykAudioPlayer.BL.SoundEngine;
 
 namespace BatykAudioPlayer.BL.FilePlaylistManager
 {
     public interface IFilePlaylistManager
     {
-        void FillSoundsDirectory(string dirPath);
-        void FillPlaylist();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dirPath"></param>
+        List<Sound> FillSoundsFromDirectory(string dirPath);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        List<Sound> FillPlaylist();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dirPath"></param>
         void SetDefaultDirectory(string dirPath);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="listPath"></param>
         void SetDefualtPlaylist(string listPath);
 
     }
