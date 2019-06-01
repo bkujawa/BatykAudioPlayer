a697ead0ef807648c0b5a28282d3f9a194d62c31
Bartosz Kujawa <bkujawa@gmail.com>
2019-06-01 14:13:39 +0200

    Removed ViewModelBase.cs from BatykAudioPlayer project. Added ViewModelBase project.

--- ViewModelBase/ViewModelBase.cs
+++ ViewModelBase/ViewModelBase.cs
@@ -6,9 +6,9 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BatykAudioPlayer.APP
+namespace BatykAudioPlayer.APP.ViewModel
 {
-    class ViewModelBase : INotifyPropertyChanged
+    public class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
